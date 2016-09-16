504650ce429c79698cb188ac528769835671b80d
Ian Agpawa <agpawaji@gmail.com>
2016-09-15 22:45:09 -0400

    seems to be working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var userAgent = (req.headers["user-agent"])
+  var leftPos = userAgent.indexOf("(") + 1
+  var rightPos = userAgent.indexOf(")")
+  var os = userAgent.slice(leftPos, rightPos)
+
+  var lang = req.headers["accept-language"].split(",")[0]
+  var ip = (req.connection.remoteAddress).split(".");
+  var firstip = ip[0].split(":")[ip[0].split(":").length - 1]
+  var formattedIP = firstip + "." + ip[1] + "." + ip[2]+ "." + ip[3]
+
+
+
+
+  res.json({ipaddress: formattedIP, language: lang, software: os });
 });
 
 module.exports = router;
